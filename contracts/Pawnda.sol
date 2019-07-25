c52aea0a57582f75655c10ee7a675baeb328d670
Clement Lakhal <clementlakhal@gmail.com>
2019-07-25 11:46:38 +0200

    Add basic funcs and tests

--- contracts/Pawnda.sol
+++ contracts/Pawnda.sol
@@ -1,4 +1,8 @@
-pragma solidity 0.5.7;
+pragma solidity 0.5.10;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 /**
@@ -6,21 +10,162 @@ pragma solidity 0.5.7;
  * @dev This contract is the base of our project
  */
 contract Pawnda {
-    uint256 private number;
+    mapping (address => uint256) public nonces;
+    mapping (bytes => bool) public canceledPawnRequests;
 
-    /**
-     * @dev Sets the number
-     * @param newNumber The new number to store
-     */
-    function setNumber(uint256 newNumber) external {
-        number = newNumber;
+    struct Pawn {
+        address customer;
+        address broker;
+        address collateralAddress;
+        uint256 collateralId;
+        address currencyAddress;
+        uint256 amount;
+        uint16 rate;
+        uint32 loanDeadline;
+        bool isClosed;
     }
 
-    /**
-     * @dev Gets the stored number
-     * @return The number as an uint
-     */
-    function getNumber() external view returns (uint256) {
-        return number;
+    Pawn[] public pawns;
+
+    function pawnCollateral(
+        address customer,
+        uint256 customerNonce,
+        address broker,
+        uint256 brokerNonce,
+        address collateralAddress,
+        uint256 collateralId,
+        address currencyAddress,
+        uint256 amount,
+        uint16 rate,
+        uint32 loanDeadline,
+        bytes calldata customerSig,
+        bytes calldata brokerSig
+    ) external {
+        require(
+            customer == getSigner(
+                customerSig,
+                customer,
+                customerNonce,
+                broker,
+                brokerNonce,
+                collateralAddress,
+                collateralId,
+                currencyAddress,
+                amount,
+                rate,
+                loanDeadline
+            ),
+            "Customer is not the signer"
+        );
+
+        require(
+            broker == getSigner(
+                brokerSig,
+                customer,
+                customerNonce,
+                broker,
+                brokerNonce,
+                collateralAddress,
+                collateralId,
+                currencyAddress,
+                amount,
+                rate,
+                loanDeadline
+            ),
+            "Broker is not the signer"
+        );
+    }
+
+    function getSigner(
+        bytes memory signature,
+        address customer,
+        uint256 customerNonce,
+        address broker,
+        uint256 brokerNonce,
+        address collateralAddress,
+        uint256 collateralId,
+        address currencyAddress,
+        uint256 amount,
+        uint16 rate,
+        uint32 loanDeadline
+    ) public pure returns (
+        address signer
+    ) {
+        bytes32 typeHash = keccak256(abi.encodePacked(
+            "address customer",
+            "uint256 customerNonce",
+            "address broker",
+            "uint256 brokerNonce",
+            "address collateralAddress",
+            "uint256 collateralId",
+            "address currencyAddress",
+            "uint256 amount",
+            "uint16 rate",
+            "uint32 loanDeadline"
+        ));
+
+        bytes32 valueHash = keccak256(abi.encodePacked(
+            customer,
+            customerNonce,
+            broker,
+            brokerNonce,
+            collateralAddress,
+            collateralId,
+            currencyAddress,
+            amount,
+            rate,
+            loanDeadline
+        ));
+
+        return recoverSigner(
+            keccak256(abi.encodePacked(typeHash, valueHash)),
+            signature
+        );
+    }
+
+    function splitSignature(
+        bytes memory signature
+    ) private pure returns (
+        uint8,
+        bytes32,
+        bytes32
+    ) {
+        require(signature.length == 65);
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        /* solhint-disable-next-line no-inline-assembly */
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+            v := byte(0, mload(add(signature, 96)))
+        }
+
+        return (v, r, s);
+    }
+
+    function recoverSigner(
+        bytes32 hash,
+        bytes memory signature
+    ) private pure returns (
+        address signer
+    ) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        (v, r, s) = splitSignature(signature);
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        if (v != 27 && v != 28) {
+            return address(0);
+        } else {
+            return ecrecover(hash, v, r, s);
+        }
     }
 }
--- test/Pawnda.test.js
+++ test/Pawnda.test.js
@@ -3,20 +3,45 @@
 
 const Pawnda = artifacts.require('Pawnda');
 
-let instance;
+let pawnda;
 
 contract('Pawnda', (accounts) => {
   it('Should deploy an instance of the Pawnda contract', () => Pawnda.deployed()
-    .then((contractInstance) => {
-      instance = contractInstance;
+    .then((instance) => {
+      pawnda = instance;
     }));
 
-  it('Should set the number', () => instance.setNumber(2, {
-    from: accounts[0],
-  }));
+  it('Should find the signer of the signature', () => pawnda.getSigner(
+    '0x18eb94f603e016b21b710baa63c0c3ff021d5bc2448a2abbbab8d1464ea944f605b0e3b0c8967d2e6b2d69750d96e0f2473e94d9402bca92092acf3fa91d8c991c',
+    '0xc1B6BFff024E52243Bb66953559c8FEA377d552c',
+    '0',
+    '0x92Cb2E864aefabf0c319aee79464e8aae70c95dD',
+    '0',
+    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
+    '42',
+    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
+    '150',
+    '1',
+    '999',
+  )
+    .then((signer) => {
+      assert.equal(signer, '0xc1B6BFff024E52243Bb66953559c8FEA377d552c', 'Signer is wrong');
+    }));
 
-  it('Should get the number', () => instance.getNumber()
-    .then((number) => {
-      assert.equal(number.toNumber(), 2, 'Number is wrong!');
+  it('Should find the other signer of the signature', () => pawnda.getSigner(
+    '0xd7f120e15a9905a963897f4c912c06f65465e40e006e10fb5365f0d39112cc666e19f38864654c00ff2b6fe98048d162c80ff43251993e806764b1faaf40f4ac1b',
+    '0xc1B6BFff024E52243Bb66953559c8FEA377d552c',
+    '0',
+    '0x92Cb2E864aefabf0c319aee79464e8aae70c95dD',
+    '0',
+    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
+    '42',
+    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
+    '150',
+    '1',
+    '999',
+  )
+    .then((signer) => {
+      assert.equal(signer, '0x92Cb2E864aefabf0c319aee79464e8aae70c95dD', 'Signer is wrong');
     }));
 });
