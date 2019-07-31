b564470b1cf07006e9e127269668ac079535c250
Clement Lakhal <clementlakhal@gmail.com>
2019-07-31 11:17:48 +0200

    Update test

--- test/Pawnda.test.js
+++ test/Pawnda.test.js
@@ -1,47 +1,73 @@
 /* eslint-env node, mocha */
-/* global artifacts, contract, it, assert */
+/* global artifacts, contract, it, assert, web3 */
 
 const Pawnda = artifacts.require('Pawnda');
+const DummyToken = artifacts.require('DummyToken');
+const DummyNifties = artifacts.require('DummyNifties');
+
+const Web3 = require('web3');
+
+const provider = new Web3('http://127.0.0.1:8545');
 
 let pawnda;
+let dummyToken;
+let dummyNifties;
+
+const data = [
+  {
+    type: 'address',
+    name: 'customer',
+    value: '0xc1B6BFff024E52243Bb66953559c8FEA377d552c',
+  },
+];
 
 contract('Pawnda', (accounts) => {
+  it('Should deploy an instance of the DummyToken contract', () => DummyToken.deployed()
+    .then((instance) => {
+      dummyToken = instance;
+    }));
+
+  it('Should deploy an instance of the DummyNifties contract', () => DummyNifties.deployed()
+    .then((instance) => {
+      dummyNifties = instance;
+    }));
+
   it('Should deploy an instance of the Pawnda contract', () => Pawnda.deployed()
     .then((instance) => {
       pawnda = instance;
     }));
 
-  it('Should find the signer of the signature', () => pawnda.getSigner(
-    '0x18eb94f603e016b21b710baa63c0c3ff021d5bc2448a2abbbab8d1464ea944f605b0e3b0c8967d2e6b2d69750d96e0f2473e94d9402bca92092acf3fa91d8c991c',
-    '0xc1B6BFff024E52243Bb66953559c8FEA377d552c',
-    '0',
-    '0x92Cb2E864aefabf0c319aee79464e8aae70c95dD',
-    '0',
-    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
-    '42',
-    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
-    '150',
-    '1',
-    '999',
-  )
-    .then((signer) => {
-      assert.equal(signer, '0xc1B6BFff024E52243Bb66953559c8FEA377d552c', 'Signer is wrong');
+  it('Should claim a test NFT', () => dummyNifties.claimFreeNifty({
+    from: accounts[0],
+  }));
+
+  it('Should check the owner of the NFT 0', () => dummyNifties.ownerOf(0)
+    .then((owner) => {
+      assert.equal(owner, accounts[0], 'Owner is wrong');
     }));
 
-  it('Should find the other signer of the signature', () => pawnda.getSigner(
-    '0xd7f120e15a9905a963897f4c912c06f65465e40e006e10fb5365f0d39112cc666e19f38864654c00ff2b6fe98048d162c80ff43251993e806764b1faaf40f4ac1b',
-    '0xc1B6BFff024E52243Bb66953559c8FEA377d552c',
-    '0',
-    '0x92Cb2E864aefabf0c319aee79464e8aae70c95dD',
-    '0',
-    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
-    '42',
-    '0x653036dDd25CeB5Ecc2D933f27e33D95C23F1043',
-    '150',
-    '1',
-    '999',
-  )
-    .then((signer) => {
-      assert.equal(signer, '0x92Cb2E864aefabf0c319aee79464e8aae70c95dD', 'Signer is wrong');
+  it('Should claim test tokens', () => dummyToken.claimFreeTokens(web3.utils.toWei('1'), {
+    from: accounts[1],
+  }));
+
+  it('Should check the balance of user 1', () => dummyToken.balanceOf(accounts[1])
+    .then((balance) => {
+      assert.equal(balance, web3.utils.toWei('1'), 'Account 1 balance is wrong');
     }));
+
+  it('Should sign a pawn request', () => {
+    provider.eth.accounts.wallet.add('0x4e99f11a187eab838e84957f3b6b7a6931baf9e253bcf661b0d6c070797299a6');
+
+    return provider.currentProvider.send({
+      method: 'eth_signTypedData',
+      params: [data, '0xC6069235b8dFF9051Cc71a4D0802427Ed7730a2E'],
+      from: '0xC6069235b8dFF9051Cc71a4D0802427Ed7730a2E',
+    }, (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+
+      console.log(result);
+    });
+  });
 });
