2e79f5b7899960bc8894114fd7f8a412918dc8e3
Clement Lakhal <clementlakhal@gmail.com>
2019-08-02 10:25:26 +0200

    Update config and migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,10 +5,12 @@ const Pawnda = artifacts.require('Pawnda');
 const DummyToken = artifacts.require('DummyToken');
 const DummyNifties = artifacts.require('DummyNifties');
 
-function deployContracts(deployer) {
-  deployer.deploy(Pawnda);
-  deployer.deploy(DummyToken);
-  deployer.deploy(DummyNifties);
+function deployContracts(deployer, network) {
+  if (network === 'ropsten' || network === 'development') {
+    deployer.deploy(Pawnda);
+    deployer.deploy(DummyToken);
+    deployer.deploy(DummyNifties);
+  }
 }
 
 module.exports = deployContracts;
